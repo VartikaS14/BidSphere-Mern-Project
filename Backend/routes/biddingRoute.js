7a9a42c95d68b86c6d8f607ff748dff7b59b1b85
vartika952 <vartika952.be22@chitkara.edu.in>
2024-11-20 00:05:35 +0530

    bidding model created

--- Backend/routes/biddingRoute.js
+++ Backend/routes/biddingRoute.js
@@ -1,13 +1,10 @@
 const express = require("express");
-const { getBiddingHistory,} = require("../controllers/biddingCtr");
-// const { protect, isSeller } = require("../middleWare/authMiddleWare");
+const { getBiddingHistory,placeBid,sellProduct} = require("../controllers/biddingCtr");
+const { protect, isSeller } = require("../middleWare/authMiddleWare");
 const router = express.Router();
 
  router.get("/:productId", getBiddingHistory);
-// router.post("/sell", protect, isSeller, sellProduct);
-// router.post("/", protect, placeBid);
+ router.post("/sell", protect, isSeller, sellProduct);
+ router.post("/", protect, placeBid);
 
 module.exports = router;
-
-
-
